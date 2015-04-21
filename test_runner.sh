c812d11fe38f40b32377c48165968ac443c8e226
Beyonce Jabroni <labs+test-bot@flatironschool.com>
2015-04-21 14:03:42 -0400

    Update test_runner.sh

--- test_runner.sh
+++ test_runner.sh
@@ -3,8 +3,7 @@ set -e
 CURR_DIR="$2";
 cd "$1";
 gunzip -c -S .xcactivitylog `ls -t | grep 'xcactivitylog' | head -n1` > output2.log;
-awk '{ gsub("", "
-"); print }' output2.log > unixfile.txt;
+awk '{ gsub("\r", "\n"); print }' output2.log > unixfile.txt;
 LOG=`echo "puts /(^Test Suite '[w-]+?.xctest' started at .+?Test Suite '[w-]+?.xctest' (failed|passed).+?.$)/m.match(File.read("unixfile.txt"))" | ruby`;
 cd "$CURR_DIR"
 [[ -s "/Users/$USER/.rvm/scripts/rvm" ]] && source "/Users/$USER/.rvm/scripts/rvm"
@@ -12,4 +11,4 @@ if [[ -z "$(which learn-xcpretty)" ]]; then
   gem install learn-xcpretty
 fi
 echo "$LOG" | learn-xcpretty -t --report learn
-
+echo "test"
